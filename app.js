a46be3bf07de37f4794d464e06cbdfeb909d7161
msumari <suleykhery@gmail.com>
2020-09-04 09:04:10 +0300

    project initialization

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('Chatroom');
+  
+})
+
+
+
+
+
+
+
+server.listen(3030,()=>console.log('server is up'));
